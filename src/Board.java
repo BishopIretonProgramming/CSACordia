b276ca729d64de0fc463aa3c1067db1344e6f627
Flambrew <andrew.matherne24@bishopireton.org>
2023-05-04 12:57:10 -0400

    added board class shell and city class

---
+++ src/Board.java
@@ -0,0 +1,12 @@
+package src;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Board {
+    ArrayList<City> cities;
+
+    public Board(File boardPreset) {
+        // TODO: Implement file to map conversion.
+    }
+}
---
+++ src/City.java
@@ -0,0 +1,74 @@
+package src;
+
+public class City {
+    private String name;
+    private double x, y;
+
+    private City[] landConnections;
+    private City[] seaConnections;
+
+    /**
+     * Creates an instance of <code>City</code>.
+     * 
+     * @param name city name
+     * @param x    x position
+     * @param y    y position
+     */
+    public City(String name, double x, double y) {
+        if (name == null)
+            throw new IllegalArgumentException("You idiot. You fool. You absolute buffoon. That don't work cuh...");
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Returns the name of this city.
+     * 
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the x position of this city.
+     * 
+     * @return x position
+     */
+    public double getX() {
+        return x;
+    }
+
+    /**
+     * Returns the y position of this city.
+     * 
+     * @return y position
+     */
+    public double getY() {
+        return y;
+    }
+
+    /**
+     * Returns a <code>City</code> array containing this city's neighbors.
+     * 
+     * @param landed landed connections or sea connections
+     * @return adjacent cities
+     */
+    public City[] getConnections(boolean landed) {
+        return landed ? landConnections : seaConnections;
+    }
+
+    /**
+     * Sets the current array of connected cities.
+     * 
+     * @param landed landed connections or sea connections
+     * @param connections cites to be connected
+     */
+    public void setConnectedCities(boolean landed, City... connections) {
+        if (landed)
+            landConnections = connections;
+        else
+            seaConnections = connections;
+    }
+}
