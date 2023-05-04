54d2a3d2a4ffff1ed1d4c7410fb8aba59f3328f3
Flambrew <andrew.matherne24@bishopireton.org>
2023-05-04 13:28:16 -0400

    added user class

--- src/Board.java
+++ src/Board.java
@@ -4,9 +4,10 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class Board {
+    ArrayList<User> users;
     ArrayList<City> cities;
 
-    public Board(File boardPreset) {
+    public Board(File boardPreset, User... users) {
         // TODO: Implement file to map conversion.
     }
 }
