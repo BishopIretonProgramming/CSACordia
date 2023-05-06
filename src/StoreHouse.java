d5101f654ac0c2e5c32279a57791b08f7ea8a292
devinlinux <ssmith969898@gmail.com>
2023-05-06 13:33:50 -0400

    Created StoreHouse class, need Good enum or class as well as Colonist class to create full implementation

---
+++ src/StoreHouse.java
@@ -0,0 +1,63 @@
+package src;
+
+//  imports
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Represents a store house that can store 12 elements
+ * can be serialized for game state saving
+ * @author Michael Bobrowski (devinlinux)
+ */
+public class StoreHouse implements java.io.Serializable {
+    
+    @java.io.Serial
+    private static final long serialVersionUID = 1L;  //  the version ID for serialization
+    private static final int MAX_CAPACITY = 12;       //  the maximum capacity of the store house
+    private List<Object> elements;                    //  the elements of the store house
+
+    //  no-args constructor to make a new store house
+    public StoreHouse() {
+        this.elements = new ArrayList<>(MAX_CAPACITY);
+        init();
+    }
+    
+    //  method to initialize the store house by adding the starting elements
+    private void init() {
+        //  TODO: Good enum/class, Colonist class
+        // elements.add(Good.WINE);
+        // elements.add(Good.BRICK);
+        // elements.add(Good.TOOL);
+        // elements.add(Good.CLOTH);
+        // elements.add(Good.FOOD);
+        // elements.add(Good.FOOD);
+        // elements.add(new Colonist(ColonistType.SEA));
+        // elements.add(new Colonist(ColonistType.SEA));
+        // elements.add(new Colonist(ColonistType.LAND));
+        // elements.add(new Colonist(ColonistType.LAND));
+    }
+
+    //  method to add an element to the StoreHouse
+    public void add(Object element) {
+        // TODO: Good enum/class, Colonist class
+        //if (element instanceof Good || element instanceof Colonist) {
+            if (elements.size() < MAX_CAPACITY) {
+                elements.add(element);
+            } else {
+                throw new IllegalStateException("StoreHouse is full");
+            }
+        //} else {
+            //throw new IllegalArgumentException("Element must be of type Good or Colonist");
+        //}
+    }
+
+    //  getter to get the number of elements
+    public int size() {
+        return elements.size();
+    }
+
+    //  getter to get the capacity of the storehouse
+    public int capacity() {
+        return MAX_CAPACITY;
+    }
+}
