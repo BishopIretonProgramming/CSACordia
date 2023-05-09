35b89edb08224ba41d53554e751ad619a4302187
devinlinux <ssmith969898@gmail.com>
2023-05-09 14:22:18 -0400

    Refractored Colonist class to use an enum instead of a boolean for easier understanding

--- src/game/Colonist.java
+++ src/game/Colonist.java
@@ -2,6 +2,8 @@ package src.game;
 
 //  imports
 import src.game.map.PathNode;
+import static src.game.Colonist.ColonistType.SEA;
+import static src.game.Colonist.ColonistType.LAND;
 
 /**
  * A class to represent a colonist
@@ -14,57 +16,64 @@ public abstract class Colonist {
 	/**
 	 * The player that the colonist is associated with
 	 */
-	private Player player;
+	private final Player PLAYER;
 
 	/**
 	 * The current path that the colonist is on
 	 */
-	private PathNode path;
+	private final PathNode PATH;
 
 	/**
-	 * whether the colonist is a land colonist
+	 * The type of colonist that this colonist is
 	 */
-	private boolean isLand;
+	private final ColonistType TYPE;
 
 	/**
 	 * Constructor to make a new Colonist
 	 * 
-	 * @param player the player that the colonist is associated with
-	 * @param path   the current path of the player
-	 * @param isLand whether the colonist is a land colonist
+	 * @param PLAYER the player that the colonist is associated with
+	 * @param PATH   the current path of the player
+	 * @param TYPE the type of colonist that this colonist is
 	 */
-	public Colonist(Player player, PathNode path, boolean isLand) {
-		this.player = player;
-		this.path = path;
-		this.isLand = isLand;
+	public Colonist(final Player PLAYER, final PathNode PATH, final ColonistType TYPE) {
+		this.PLAYER = PLAYER;
+		this.PATH = PATH;
+		this.TYPE = TYPE;
 	}
 
 	/**
 	 * Getter to get the player that this Colonist is associated with
 	 */
 	public Player player() {
-		return this.player;
+		return this.PLAYER;
 	}
 
 	/**
-	 * Getter to return the PathNode that this colonis is on
+	 * Getter to return the PathNode that this colonist is on
 	 */
 	public PathNode path() {
-		return this.path;
+		return this.PATH;
+	}
+
+	/**
+	 * Getter method to return the type of colonist this is
+	 */
+	public ColonistType type() {
+		return this.TYPE;
 	}
 
 	/**
 	 * Getter to return whether this colonist is a land colonist
 	 */
 	public boolean isLand() {
-		return this.isLand;
+		return this.TYPE == LAND;
 	}
 
 	/**
 	 * Getter to return whether this colonist is a sea colonist
 	 */
 	public boolean isSea() {
-		return !this.isLand;
+		return this.TYPE == SEA;
 	}
 
 	/**
@@ -72,4 +81,12 @@ public abstract class Colonist {
 	 * @param node the PathNode to move to
 	 */
 	public abstract boolean canMoveTo(PathNode node);
+
+	/**
+	 * An enum to represent the type of colonist that a colonist is
+	 */
+	public enum ColonistType {
+		SEA,
+		LAND
+	}
 }
