3d621d2e3858dc34a58bc7dee9df6118ed414032
SlicedIceTea <genericuserj@gmail.com>
2023-05-10 08:35:54 -0400

    Province and ProvinceChart

---
+++ src/game/Province.java
@@ -0,0 +1,82 @@
+package src.game;
+
+/*
+   Joseph Murray
+   5/9/2023
+   Province - representation of game provinces, used for good collection from tribune cards
+*/
+
+import java.util.ArrayList;
+
+public class Province {
+   private ArrayList<City> cityList = new ArrayList<City>();
+   private String name;
+   private Good provinceGood;
+   private ResourceStatus status;
+   private int provinceSestercii;
+   
+   public enum ResourceStatus {
+      GOODS,
+      SESTERCII
+   }
+   
+                                             //METHODS
+                                             
+   public Province(String name, ArrayList<City> cityList) {
+      this.name = name;
+      this.cityList = cityList;
+      
+      status = ResourceStatus.GOODS;
+   }
+   
+   public int collectSestercii() {
+      status = ResourceStatus.GOODS;
+      
+      return provinceSestercii;
+   }
+   
+   //TO-DO: create way for Province to check which cities players own, and collect goods accordingly
+   public ArrayList<Good> collectGoods() {
+      status = ResourceStatus.SESTERCII;
+      
+      ArrayList<Good> goodsCollected = new ArrayList<Good>();      
+      goodsCollected.add(provinceGood);
+      
+      for(City city : cityList) {
+         //goodsCollected.add(city.getGood());
+      }
+      
+      return goodsCollected;
+   }
+   
+   private Good setProvinceGood() {
+      Good good = Good.BRICK;
+      
+      //TO-DO: find a way to implement this once these methods are available in City and Good classes
+      /*for(City city : cityList) {
+         if(city.getGood().PRICE > good.Price) {
+            good = city.getGood();
+         }
+      }*/
+      
+      return good;
+   }
+   
+                                             //GETTERS - SETTERS
+                                             
+   public String getName() {
+      return name;
+   }
+   
+   public ResourceStatus getStatus() {
+      return status;
+   }
+   
+   public void setGood(Good good) {
+      provinceGood = good;
+   }
+   
+   public void setSestercii(int sestercii) {
+      provinceSestercii = sestercii;
+   }
+}
---
+++ src/game/ProvinceChart.java
@@ -0,0 +1,85 @@
+package src.game;
+
+/*
+   Joseph Murray
+   5/9/2023
+   ProvinceGoodsChart - The lil chart with province goods
+*/
+
+import java.util.ArrayList;
+
+public class ProvinceChart {
+   private static ProvinceChart provinceChartInstance = null;
+   private ArrayList<Province> provinceList = new ArrayList<Province>();
+   
+   private ProvinceChart() {
+      addProvinces();
+   }
+   
+   //method ensures only one instance of ProvinceChart can be created
+   public static ProvinceChart getInstance() {
+      if(provinceChartInstance == null) {
+         provinceChartInstance = new ProvinceChart();
+      }
+      
+      return provinceChartInstance;
+   }
+   
+                                             //METHODS
+
+   //Collect goods or sestercii when tribune, prefect or prefectus Magnus are used
+   //@Param: the province name as a String, can later modify to accept Province objects if needed
+   public void collect(String provinceName) {
+      Province province = findProvince(provinceName);
+      
+      if(province.getStatus() == Province.ResourceStatus.GOODS) {
+         ArrayList<Good> goods = collectGoods(province);
+      }
+      else {
+         int sestercii = collectSestercii();
+      }
+   }
+   
+   private int collectSestercii() {
+      int sestercii = 0;
+      
+      for(Province province : provinceList) {
+         if(province.getStatus() == Province.ResourceStatus.SESTERCII) {
+            sestercii += province.collectSestercii();
+         }
+      }
+      
+      return sestercii;
+   }
+   
+   private ArrayList<Good> collectGoods(Province province) {
+      if(province.getStatus() == Province.ResourceStatus.GOODS) {
+          return province.collectGoods();
+      }
+   }
+   
+   //takes the province name and finds the corresponding Province object
+   private Province findProvince(String provinceName) {
+      for(Province province : provinceList) {
+         if(provinceName == province.getName()) {
+            return province;
+         }
+      }
+   }
+   
+   //TO-DO: add cities, either in this class or passed from another
+   private void addProvinces() {
+      provinceList.add(new Province("Britania", new ArrayList<City>(2)));
+      provinceList.add(new Province("Gallia", new ArrayList<City>(3)));
+      provinceList.add(new Province("Hispania", new ArrayList<City>(3)));
+      provinceList.add(new Province("Germania", new ArrayList<City>(2)));
+      provinceList.add(new Province("Dacia", new ArrayList<City>(3)));
+      provinceList.add(new Province("Italia", new ArrayList<City>(3)));
+      provinceList.add(new Province("Hellas", new ArrayList<City>(2)));
+      provinceList.add(new Province("Asia", new ArrayList<City>(3)));
+      provinceList.add(new Province("Syria", new ArrayList<City>(2)));
+      provinceList.add(new Province("Aegyptus", new ArrayList<City>(3)));
+      provinceList.add(new Province("Lybia", new ArrayList<City>(2)));
+      provinceList.add(new Province("Mauretania", new ArrayList<City>(2)));
+   }
+}
