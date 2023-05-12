514da76de293e75ab188b0fa2de883f7576a4b9e
devinlinux <ssmith969898@gmail.com>
2023-05-12 12:49:47 -0400

    Made changes to Good formatting

--- src/game/Good.java
+++ src/game/Good.java
@@ -8,7 +8,7 @@ package src.game;
 * String NAME
 */
 
-public enum Good{
+public enum Good {
 
    WINE(6,"wine"), 
    BRICK(3,"brick"),
@@ -16,10 +16,22 @@ public enum Good{
    CLOTH(7,"cloth"), 
    FOOD(4,"food");
    
+   /**
+    * The price of the good
+    */
    final int PRICE;
+
+   /**
+    * The name of the good
+    */
    final String NAME; 
    
-   Good(int v, String n){
+   /**
+    * Constructor to make a new Good
+    * @param v the price of the Good
+    * @param n the name of the good
+    */
+   Good(int v, String n) {
       PRICE = v;
       NAME = n;
    }
