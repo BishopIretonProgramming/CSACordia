e8f882f68fd0cad73c1c8a0036f160a0d27c850e
devinlinux <ssmith969898@gmail.com>
2023-05-12 13:06:58 -0400

    Made Main class to run the program, make sure that this is the main method that contains the play loop

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+package src;
+
+/**
+ * Class to run the game, use this main method for the main run loop
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        //  Use this for the main play loop
+        //  init graphics and make the Game here
+    }
+}
