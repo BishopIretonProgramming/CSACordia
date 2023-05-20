048b50f3e6723b2f1179a32d3204ce78b12a6e25
SlicedIceTea <genericuserj@gmail.com>
2023-05-20 08:38:00 -0400

    Added JButtons and began working on Image loading

--- src/gui/PlayerDeck.java
+++ src/gui/PlayerDeck.java
@@ -7,7 +7,110 @@ package src.gui;
 */
 
 import src.game.Player;
+import src.game.cards.*;
+
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.JPanel;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.util.ArrayList;
 
 public class PlayerDeck {
-    
+    Player player;
+
+    ArrayList<PersonalityCard> cards;
+    ArrayList<PersonalityCard> discardCards;
+    PersonalityCard shownCard;
+
+    ArrayList<BufferedImage> cardImages;
+
+    JButton leftButton;
+    JButton rightButton;
+    JButton infoButton;
+    JButton useButton;
+
+    JRadioButton cardFilters;
+    JPanel panel;
+
+    public PlayerDeck(Player player, ArrayList<PersonalityCard> cards, JPanel panel) {
+        this.player = player;
+        this.cards = cards;
+        this.panel = panel;
+
+        discardCards = new ArrayList<PersonalityCard>(cards.size());
+
+        createButtons();
+    }
+
+    private void loadStartImages() {
+        //TODO: call loadImage() for all starting images
+    }
+
+    private void createButtons() {
+        leftButton = new JButton();
+        rightButton = new JButton();
+        infoButton = new JButton();
+        useButton = new JButton();
+
+        panel.add(leftButton);
+        panel.add(rightButton);
+        panel.add(infoButton);
+        panel.add(useButton);
+
+        leftButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                changeCard(-1);
+            }
+        });
+
+        rightButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                changeCard(1);
+            }
+        });
+
+        infoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Show information");
+            }
+        });
+
+        useButton.addActionListener(new ActionListener() {
+            @Override            
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Use card");
+            }
+        });
+    }
+
+    private void addStartCards(ArrayList<PersonalityCard> cards) {}
+
+    private void loadImage(BufferedImage image) {
+        try {
+            //load image
+            //TODO: create images of buttons and cards
+        }
+        catch(IOException e) {
+            System.out.println("Failed to load image");
+        }
+    }
+
+    private void changeCard(int indexChange) {}
+    private void addCard(PersonalityCard card) {}
+    private void removeCard(PersonalityCard card) {}
+
+    public void drawGraphics(Graphics g) {
+        
+    }
 }
