a228560c1a7e8879f26b8dec1c92c96044878ce2
devinlinux <ssmith969898@gmail.com>
2023-05-21 19:03:54 -0400

    Added fields and methods to deal with the occupancy status of a path

--- src/game/map/PathNode.java
+++ src/game/map/PathNode.java
@@ -42,6 +42,11 @@ public class PathNode implements java.io.Serializable {
     private PathType type;
 
     /**
+     * Whether this PathNode (path) is currently occupied by a Colonist
+     */
+    private boolean occupied;
+
+    /**
      * An enum to represent the two types of PathNodes - land or sea
      */
     public enum PathType {
@@ -112,6 +117,7 @@ public class PathNode implements java.io.Serializable {
         this.node1 = node1;
         this.node2 = node2;
         this.type = type;
+        this.occupied = false;
     }
 
     /**
@@ -182,6 +188,22 @@ public class PathNode implements java.io.Serializable {
         } catch (IOException e) {
             System.err.printf("Error writing PathNodes to file: %s%n", e.getMessage());
         }
+    }
+
+    /**
+     * Getter to return whether this PathNode (path) is occupied by a Colonist
+     * @return whether this PathNode (path) is occupied by a Colonist
+     */
+    public boolean occupied() {
+        return this.occupied;
+    }
+
+    /**
+     * Setter to set whether this PathNode (path) is occupied by a Colonist
+     * @param occupied whether this PathNode (path) is occupied by a Colonist
+     */
+    public void setOccupied(boolean occupied) {
+        this.occupied = occupied;
     }
 
     /**
