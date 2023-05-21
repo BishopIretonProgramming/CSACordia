697e6dd97e4e9bec212ab2fece463a370ae9aa6a
SlicedIceTea <genericuserj@gmail.com>
2023-05-21 18:31:27 -0400

    Added RadioButtons and JLabels, commented code completely, added a few helpful getters, rewrote and restructered most of the methods

--- src/gui/PlayerDeck.java
+++ src/gui/PlayerDeck.java
@@ -6,19 +6,21 @@ package src.gui;
     PlayerDeck: GUI representation of the players hand of personality cards
 */
 
+//TODO: add positioning and sizing for all GUI components
+
 import src.game.Player;
 import src.game.cards.*;
 
 import javax.swing.JButton;
 import javax.swing.JRadioButton;
-
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 import java.awt.Graphics;
 import java.awt.Insets;
-
 import java.awt.image.BufferedImage;
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -26,19 +28,34 @@ import java.io.IOException;
 import java.io.File;
 
 import javax.imageio.ImageIO;
+
 import java.util.ArrayList;
 
 public class PlayerDeck {
-    private Player player;
+    private Player player; //The player whose deck this is
 
-    private ArrayList<PersonalityCard> cards;
-    private ArrayList<PersonalityCard> discardCards;
-    private PersonalityCard shownCard;
-    private int shownCardID;
+    private ArrayList<PersonalityCard> cards; //The cards the player can play
+    private ArrayList<PersonalityCard> discardCards; //The cards the player have already played
+    private PersonalityCard shownCard; //The card that is currently displayed and visible in PlayerHandDisplay
+    private int shownCardID; //The index of the shownCard; used for changing cards
 
-    private ArrayList<BufferedImage> cardImages;
-    private String[] cardImageFileNames = { //TODO: add praefectus magnus image and class
+    private ArrayList<BufferedImage> cardImages; //List of card images
+    private ArrayList<BufferedImage> guiImages; //List of button and label images
+
+    private String[] guiImageFileNames = { //list of file names, temporary until file reading added //TODO: add images of buttons and labels
+        " ",
+        " ",
+        " ",
+        " ",
+        " ",
+        " ",
+        " ",
+        " ",
+        " "
+    };
+    private String[] cardImageFileNames = { //list of file names, temporary until file reading added //TODO: add praefectus magnus image and class
         "architect.png",
+        "colonist.png",
         "consul.png",
         "diplomat.png",
         "mercator.png",
@@ -49,39 +66,27 @@ public class PlayerDeck {
         "weaver.png"
     };
 
-    private JButton leftButton;
-    private JButton rightButton;
-    private JButton infoButton;
-    private JButton useButton;
+    private JButton leftButton; //Button to cycle left through cards
+    private JButton rightButton; //Button to cycle right through cards
+    private JButton infoButton; //Button to display more info on shownCard
+    private JButton useButton; //Button to play the shownCard
 
-    private final String LEFT_BUTTON_IMAGE_FILE_NAME = "";
-    private final String RIGHT_BUTTON_IMAGE_FILE_NAME = "";
-    private final String INFO_BUTTON_IMAGE_FILE_NAME = "";
-    private final String USE_BUTTON_IMAGE_FILE_NAME = "";
+    private ButtonGroup cardFilters; //Selection to select which type of cards are shown
+    private JRadioButton shownCards; //shows cards the player can play (see cards)
+    private JRadioButton discardedCards; //shows cards the player has already played (see discardedCards)
 
-    private int leftButtonWidth = 50;
-    private int leftButtonHeight = 50;
-    private int leftButtonX = 0;
-    private int leftButtonY = 0;
+    private JLabel infoLabel; //Label that displays info on the shownCard
+    private JLabel backgroundLabel; //Label that displays a nice background for the PlayerDeck section
+    private JLabel cardLabel; //Label that displays the shownCard
 
-    private int rightButtonWidth = 50;
-    private int rightButtonHeight = 50;
-    private int rightButtonX = 0;
-    private int rightButtonY = 0;
+    private JPanel panel; //The JPanel that holds these graphics
 
-    private int infoButtonWidth = 50;
-    private int infoButtonHeight = 150;
-    private int infoButtonX = 0;
-    private int infoButtonY = 0;
-
-    private int useButtonWidth = 100;
-    private int useButtonHeight = 50;
-    private int useButtonX = 0;
-    private int useButtonY = 0;
-
-    private JRadioButton cardFilters;
-    private JPanel panel;
-
+    /**
+     * Constructor
+     * @param player the player
+     * @param cards the starting cards the player receives
+     * @param panel the panel that holds these graphics
+    */
     public PlayerDeck(Player player, ArrayList<PersonalityCard> cards, JPanel panel) {
         this.player = player;
         this.cards = cards;
@@ -89,30 +94,25 @@ public class PlayerDeck {
 
         discardCards = new ArrayList<PersonalityCard>(cards.size());
 
+        cardImages = loadImage(cardImageFileNames);
+        guiImages = loadImage(guiImageFileNames);
+
+        infoLabel = createLabel(guiImages.get(0), 0, 0, 0, 0, "info");
+        backgroundLabel = createLabel(guiImages.get(0), 0, 0, 0, 0, "");
+        cardLabel = createLabel(guiImages.get(0), 0, 0, 0, 0, "");
+
         createButtons();
-        addCardImages();
+        createRadioButtons();
     }
 
+    /**
+     * Helper method for Constructor
+    */
     private void createButtons() {
-        leftButton = new JButton(loadImage(LEFT_BUTTON_IMAGE_FILE_NAME));
-        rightButton = new JButton(loadImage(RIGHT_BUTTON_IMAGE_FILE_NAME));
-        infoButton = new JButton(loadImage(INFO_BUTTON_IMAGE_FILE_NAME));
-        useButton = new JButton(loadImage(USE_BUTTON_IMAGE_FILE_NAME));
-
-        panel.add(leftButton);
-        panel.add(rightButton);
-        panel.add(infoButton);
-        panel.add(useButton);
-
-        leftButton.setBounds(leftButtonX, leftButtonY, leftButtonWidth, leftButtonHeight);
-        rightButton.setBounds(rightButtonX, rightButtonY, rightButtonWidth, rightButtonHeight);
-        infoButton.setBounds(infoButtonX, infoButtonY, infoButtonWidth, infoButtonHeight);
-        useButton.setBounds(useButtonX, useButtonY, useButtonWidth, useButtonHeight);
-
-        leftButton.setMargin(new Insets(-1, -1, -1, -1));
-        rightButton.setMargin(new Insets(-1, -1, -1, -1));
-        infoButton.setMargin(new Insets(-1, -1, -1, -1));
-        useButton.setMargin(new Insets(-1, -1, -1, -1));
+        leftButton = createButton(guiImages.get(0), 0, 0, 0, 0, "");
+        rightButton = createButton(guiImages.get(0), 0, 0, 0, 0, "");
+        infoButton = createButton(guiImages.get(0), 0, 0, 0, 0, "");
+        useButton = createButton(guiImages.get(0), 0, 0, 0, 0, "");
 
         infoButton.setMultiClickThreshhold(50);
         useButton.setMultiClickThreshhold(50);
@@ -131,7 +131,7 @@ public class PlayerDeck {
             }
         });
 
-        infoButton.addActionListener(new ActionListener() {
+        infoButton.addActionListener(new ActionListener() {//TODO: create info cards for each card
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Show information");
@@ -141,17 +141,129 @@ public class PlayerDeck {
         useButton.addActionListener(new ActionListener() {
             @Override            
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Use card");
+                discardCard(shownCard);
+                changeCard(1);
+
+                shownCard.doAction();
             }
         });
     }
 
-    private void addCardImages() {
-        for(String cardImageFileName : cardImageFileNames) {
-            cardImages.add(loadImage(cardImageFileName));
-        }
+    /**
+     * Helper method for Constructor 
+    */
+    private void createRadioButtons() {
+        cardFilters = new ButtonGroup();
+
+        shownCards = createRadioButton(guiImages.get(0), 0, 0, 0, 0, "Available cards");
+        discardedCards = createRadioButton(guiImages.get(0), 0, 0, 0, 0, "Discarded cards");
+
+        cardFilters.add(shownCards);
+        cardFilters.add(discardedCards);
+
+        shownCards.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("show shown cards");
+            }
+        });
+
+        discardedCards.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("show discarded cards");
+            }
+        });
+    } 
+
+    /**
+     * Helper method that initializes common values and settings for JButtons
+     * @param image image displayed by JButton, what it looks like
+     * @param xPosition 
+     * @param yPosition
+     * @param width
+     * @param height
+     * @param text text displayed by JButton
+     * @return created JButton
+    */
+    private JButton createButton(BufferedImage image, int xPosition, int yPosition, int width, int height, String text) {
+        JButton button = new JButton();
+
+        button.setIcon(new ImageIcon(image));
+        button.setBounds(xPosition, yPosition, width , height);
+        button.setText(text);
+        button.setMargin(new Insets(-1, -1, -1, -1));
+
+        panel.add(button);
+
+        return button;
     }
 
+    /**
+     * Helper method that initializes common values and settings for JRadioButtons
+     * @param image image displayed by JRadioButton, what it looks like
+     * @param xPosition
+     * @param yPosition
+     * @param width
+     * @param height
+     * @param text text displayed by JRadioButton
+     * @return created JRadioButton
+    */
+    private JRadioButton createRadioButton(BufferedImage image, int xPosition, int yPosition, int width, int height, String text) {
+        JRadioButton radioButton = new JRadioButton();
+
+        radioButton.setIcon(new ImageIcon(image));
+        radioButton.setBounds(xPosition, yPosition, width , height);
+        radioButton.setText(text);
+        radioButton.setMargin(new Insets(-1, -1, -1, -1));
+
+        panel.add(radioButton);
+
+        return radioButton;
+    }
+
+    /**
+     * Helper method that initializes common values and seetings for JLabels
+     * @param image image displayed by JLabel, what it looks like
+     * @param xPosition
+     * @param yPosition
+     * @param width
+     * @param height
+     * @param text text displayed by JLabel
+     * @return created JLabel
+    */
+    private JLabel createLabel(BufferedImage image, int xPosition, int yPosition, int width, int height, String text) {
+        JLabel label = new JLabel();
+
+        label.setIcon(new ImageIcon(image));
+        label.setBounds(xPosition, yPosition, width, height);
+        label.setText(text);
+
+        panel.add(label);
+
+        return label;
+    }
+
+    /**
+     * loads an array of image files into BufferedImages
+     * @param imageFileNames list of file names of images
+     * @return an array of BufferedImages
+    */
+    private ArrayList<BufferedImage> loadImage(String[] imageFileNames) {
+        ArrayList<BufferedImage> imageList = new ArrayList<BufferedImage>(imageFileNames.length);
+
+        for(String fileName : imageFileNames) {
+            imageList.add(loadImage(fileName));
+        }
+
+        return imageList;
+    }
+
+    /**
+     * loads an image file into a BufferedImage
+     * @param fileName file name of image
+     * @return BufferedImage
+    */
     private BufferedImage loadImage(String fileName) {
         BufferedImage image = null;
 
@@ -165,6 +277,10 @@ public class PlayerDeck {
         return image;
     }
 
+    /**
+     * changes the shownCard
+     * @param indexChange the change in the shownCard, 1 to go left one, -1 to go right one
+    */
     private void changeCard(int indexChange) {
         int newShownCardID = shownCardID + indexChange;
 
@@ -179,15 +295,56 @@ public class PlayerDeck {
         shownCard = cards.get(shownCardID);
     }
 
-    private void addCard(PersonalityCard card) {
+    /**
+     * adds a card to the player's deck, adds it to cards
+     * @param card Personality card to be added
+    */
+    public void addCard(PersonalityCard card) {
         cards.add(card);
     }
 
+    /**
+     * moves a card to the discard pile, removes it from cards, adds it to discardedCards
+     * @param card Personality card that is to be moved
+    */
+    private void discardCard(PersonalityCard card) {
+        cards.remove(shownCardID);
+        discardCards.add(card);
+    }
+
+    /**
+     * removes a card from the player's deck entirely (not to be confused with discardCard, which moves it to the discard pile)
+     * @param card Personality card to be removed
+    */
     private void removeCard(PersonalityCard card) {
         cards.remove(card);
     }
 
+    /**
+     * draws and displays these graphics
+    */
     public void drawGraphics(Graphics g) {
-        
+        //TODO: drawGraphics method
+    }
+
+    /**
+     * @return the last played card, the one at the top of the discard pile
+    */
+    public PersonalityCard getLastDiscardedCard() {
+        return discardCards.get(discardCards.size() - 1);
+    }
+
+    /**
+     * @return this players cards, not including those in the discard pile
+    */
+    public ArrayList<PersonalityCard> getCards() {
+        return cards;
+    }
+
+    /**
+     * @return the Player that this deck belongs to
+    */
+    public Player getPlayer() {
+        return player;
     }
 }
