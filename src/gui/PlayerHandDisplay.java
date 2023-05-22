cfa059f32a24d1c3ece4211131eaf5098e66fc2e
nhixson314 <noraHixson25@bishopireton.org>
2023-05-22 18:34:21 -0400

    handDisplay is a jpanel still working on it

--- src/gui/PlayerHandDisplay.java
+++ src/gui/PlayerHandDisplay.java
@@ -1,23 +1,25 @@
 package src.gui;
 import javax.swing.*;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 
-public class PlayerHandDisplay {
+public class PlayerHandDisplay extends JPanel{
 
 public static boolean isVisible;// allows other methods to turn panel on and off
 public static JPanel handDisplay = new JPanel(); // this allows other methods to acess the panel
-public static JFrame window;   
-    public static void display(JFrame w){// makes the display or panel
+//public static JFrame window;   
 
-        window = w;
-        handDisplay.setBackground(Color.BLACK);
+    public static void display(){// makes the display or panel
+
+        handDisplay.setBackground(Color.GRAY);
+        handDisplay.setLayout(new BorderLayout());
+        JLabel money = new JLabel("<html><font  size ='15' color = white> Sestertii:  </font></html>");// the html just changes the size and color should work on everyone's computer
+        JLabel storeTitle = new JLabel("<html><font  size ='20' color = white> Storehouse  </font></html>");
+        handDisplay.add(money, BorderLayout.WEST);
+        handDisplay.add(storeTitle, BorderLayout.WEST);
 
         handDisplay.setVisible(isVisible);
-        
-        
-        System.out.println("display method works");
-        JLabel test = new JLabel("test visible");
-        window.getContentPane().add(test);
     }
 
     public static void changeVisible(){
