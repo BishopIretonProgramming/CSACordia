63408582c7eb27032a098d84de7d0c99fcc0ac5d
nhixson314 <nora.hixson25@bishopireton.org>
2023-05-22 12:43:14 -0400

    It runs

--- src/gui/PlayerHandDisplay.java
+++ src/gui/PlayerHandDisplay.java
@@ -6,21 +6,24 @@ public class PlayerHandDisplay {
 
 public static boolean isVisible;// allows other methods to turn panel on and off
 public static JPanel handDisplay = new JPanel(); // this allows other methods to acess the panel
-    
-    public static void display(){// makes the display or panel
+public static JFrame window;   
+    public static void display(JFrame w){// makes the display or panel
 
+        window = w;
         handDisplay.setBackground(Color.BLACK);
 
         handDisplay.setVisible(isVisible);
         
-
-        System.out.println("display method works");
-        JLabel wLabel = new JLabel("test visible");
         
+        System.out.println("display method works");
+        JLabel test = new JLabel("test visible");
+        window.getContentPane().add(test);
     }
 
     public static void changeVisible(){
         isVisible = !isVisible;
         System.out.println( isVisible);
     }
+
+
 }
