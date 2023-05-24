85d940f24cf230aa9be61a10339f9c81ae006317
devinlinux <ssmith969898@gmail.com>
2023-05-24 11:48:37 -0400

    Continued work on ai game implementation

--- src/ai/environment/game/Colonist.java
+++ src/ai/environment/game/Colonist.java
@@ -1,9 +1,17 @@
 package src.ai.environment.game;
 
+//  imports
+import src.ai.environment.game.player.Player;
+
 /**
  * A basic class to represent a colonist in the game of Concordia.
  *
  * @author devinlinux
  */
 public class Colonist implements Storeable {
+
+    /**
+     * The player who owns this colonist
+     */
+    private Player player;
 }
