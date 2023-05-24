2361f209c21a886d562881c74107bd3171407dde
nhixson314 <noraHixson25@bishopireton.org>
2023-05-23 20:10:32 -0400

    Deleted weird files added to PlayerHandDisplay

--- src/gui/PlayerHandDisplay.java
+++ src/gui/PlayerHandDisplay.java
@@ -1,29 +1,59 @@
 package src.gui;
-import javax.swing.*;
 
-import java.awt.BorderLayout;
+//imports 
+import javax.swing.*;
 import java.awt.Color;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import src.game.Player;
+
+/* @author Nora Hixson
+ * a class to display the Jpanel with the players storehouse, Sesterii, and mabye there deck of cards
+ */
 
 public class PlayerHandDisplay extends JPanel{
 
 public static boolean isVisible;// allows other methods to turn panel on and off
 public static JPanel handDisplay = new JPanel(); // this allows other methods to acess the panel
-//public static JFrame window;   
+
+public Player currentPlayer;
+   
+// this  constructor should allow for different values to be displayed for different players
+
+// constructor
+    private PlayerHandDisplay(Player pl){
+        currentPlayer = pl; 
+    }
 
     public static void display(){// makes the display or panel
 
-        handDisplay.setBackground(Color.GRAY);
-        handDisplay.setLayout(new BorderLayout());
-        JLabel money = new JLabel("<html><font  size ='15' color = white> Sestertii:  </font></html>");// the html just changes the size and color should work on everyone's computer
-        JLabel storeTitle = new JLabel("<html><font  size ='20' color = white> Storehouse  </font></html>");
-        handDisplay.add(money, BorderLayout.WEST);
-        handDisplay.add(storeTitle, BorderLayout.WEST);
+        PlayerHandDisplay hand = new PlayerHandDisplay(null);
 
+        // sets format and layout of Panel
+
+        handDisplay.setBackground(Color.GRAY);
+        handDisplay.setLayout(new GridBagLayout());
+        GridBagConstraints mgb = new GridBagConstraints();
+        GridBagConstraints sgb = new GridBagConstraints();
+        mgb.anchor = GridBagConstraints.WEST;
+        sgb.anchor = GridBagConstraints.BASELINE;
+
+        // creates JLables
+        JLabel money = new JLabel("<html><font  size ='20' color = white> Sestertii:  </font></html>");// the html just changes the size and color should work on everyone's computer
+        JLabel storeTitle = new JLabel("<html><font  size ='20' color = white> Storehouse Contents:  </font></html>");
+        
+        //Adds JLabels to Panel
+        handDisplay.add(money,mgb);
+        handDisplay.add(storeTitle,sgb);
+
+        //Makes sure the JPanel starts as invisble
         handDisplay.setVisible(isVisible);
+        
     }
 
     public static void changeVisible(){
         isVisible = !isVisible;
+        handDisplay.setVisible(isVisible);
         System.out.println( isVisible);
     }
 
