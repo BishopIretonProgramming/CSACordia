6ba91f2035b0223574555ff3c6c68060c3e32b9e
devinlinux <ssmith969898@gmail.com>
2023-06-01 19:19:43 -0400

    New AI Plan

--- src/ai/main/Main.java
+++ src/ai/main/Main.java
@@ -1,9 +1,16 @@
 package src.ai.main;
 
+//  imports
+import src.ai.model.data.csv.CSVConfig;
+
 /**
  * The class that will start and control the training of the AI.
  *
  * @author devinlinux
  */
 public class Main {
+
+    String path = "";
+
+    
 }
