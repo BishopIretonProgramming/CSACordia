f542f345261025e5f6ebace9caab912fedf609f1
SlicedIceTea <genericuserj@gmail.com>
2023-06-03 12:40:36 -0400

    Added JLabel images to PlayerStoreHouse, Moved graphics helper methods from PlayerDeck to GUITools

--- src/gui/PlayerStoreHouse.java
+++ src/gui/PlayerStoreHouse.java
@@ -23,18 +23,19 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class PlayerStoreHouse {
+
+                                        /* Fields */
+
     private Player player;
     private StoreHouse storeHouse;
     private JPanel panel;
 
     private ArrayList<BufferedImage> slotImages;
-
-    /*
-     * 1 4 5 8 9 12
-     * 2 3 6 7 10 11
-     */
+    private ArrayList<BufferedImage> guiImages;
 
     private JLabel storeHouseImage;
+
+    //the twelve slots that display items in the storehouse
     private JLabel slotOne;
     private JLabel slotTwo;
     private JLabel slotThree;
@@ -50,9 +51,64 @@ public class PlayerStoreHouse {
 
     private final FileReaderTools SAVE_FILE = new FileReaderTools("player_deck_data");
 
+                                        /* Constructors */
+
+    /**
+     * Constructor
+     * @param player the player the storehouse belongs to
+     * @param panel the content pane that holds this graphic display
+    */
     public PlayerStoreHouse(Player player, JPanel panel) {
         this.player = player;
         this.panel = panel;
         storeHouse = player.storeHouse();
+
+        slotImages = SAVE_FILE.getImages("");
+        guiImages = SAVE_FILE.getImages("");
+
+        createLabels();
     }
+
+                                        /* Methods */
+
+    /**
+     * helper method for constructor, creates all the labels
+    */
+    private void createLabels() {
+        storeHouseImage = GUITools.createLabel(panel, guiImages.get(0), 100, 100, 100, 100);
+
+        slotOne = GUITools.createLabel(panel, slotImages.get(0), 100, 100, 100, 100);
+        slotTwo = GUITools.createLabel(panel, slotImages.get(1), 100, 100, 100, 100);
+        slotThree = GUITools.createLabel(panel, slotImages.get(2), 100, 100, 100, 100);
+        slotFour = GUITools.createLabel(panel, slotImages.get(3), 100, 100, 100, 100);
+        slotFive = GUITools.createLabel(panel, slotImages.get(4), 100, 100, 100, 100);
+        slotSix = GUITools.createLabel(panel, slotImages.get(5), 100, 100, 100, 100);
+        slotSeven = GUITools.createLabel(panel, slotImages.get(6), 100, 100, 100, 100);
+        slotEight = GUITools.createLabel(panel, slotImages.get(7), 100, 100, 100, 100);
+        slotNine = GUITools.createLabel(panel, slotImages.get(8), 100, 100, 100, 100);
+        slotTen = GUITools.createLabel(panel, slotImages.get(9), 100, 100, 100, 100);
+        slotEleven = GUITools.createLabel(panel, slotImages.get(10), 100, 100, 100, 100);
+        slotTwelve = GUITools.createLabel(panel, slotImages.get(11), 100, 100, 100, 100);
+    }
+
+    /**
+    * updates the storehouse display based on the current contents of the player's storehouse
+    */
+    public void updateStoreHouse() {
+        slotOne.setIcon(new ImageIcon(slotImages.get(0)));
+        slotTwo.setIcon(new ImageIcon(slotImages.get(1)));
+        slotThree.setIcon(new ImageIcon(slotImages.get(2)));
+        slotFour.setIcon(new ImageIcon(slotImages.get(3)));
+        slotFive.setIcon(new ImageIcon(slotImages.get(4)));
+        slotSix.setIcon(new ImageIcon(slotImages.get(5)));
+        slotSeven.setIcon(new ImageIcon(slotImages.get(6)));
+        slotEight.setIcon(new ImageIcon(slotImages.get(7)));
+        slotNine.setIcon(new ImageIcon(slotImages.get(8)));
+        slotTen.setIcon(new ImageIcon(slotImages.get(9)));
+        slotEleven.setIcon(new ImageIcon(slotImages.get(10)));
+        slotTwelve.setIcon(new ImageIcon(slotImages.get(11)));
+    }
+
+                                        /* Getters / Setters / ToString */
+
 }
