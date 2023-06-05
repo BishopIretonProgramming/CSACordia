ab5b242c547ae6cd8a2bf8e496a255bafe00de47
SlicedIceTea <genericuserj@gmail.com>
2023-06-05 11:20:50 -0400

    updated PlayerStoreHouse to use new PieceImage enum

--- src/gui/PlayerStoreHouse.java
+++ src/gui/PlayerStoreHouse.java
@@ -29,9 +29,6 @@ public class PlayerStoreHouse {
     private StoreHouse storeHouse;
     private JPanel panel;
 
-    private ArrayList<BufferedImage> slotImages;
-    private ArrayList<BufferedImage> guiImages;
-
     private JLabel storeHouseImage;
 
     //the twelve slots that display items in the storehouse
@@ -62,9 +59,6 @@ public class PlayerStoreHouse {
         this.panel = panel;
         storeHouse = player.storeHouse();
 
-        slotImages = SAVE_FILE.getImages("");
-        guiImages = SAVE_FILE.getImages("");
-
         createLabels();
     }
 
@@ -74,38 +68,38 @@ public class PlayerStoreHouse {
      * helper method for constructor, creates all the labels
     */
     private void createLabels() {
-        storeHouseImage = GUITools.createLabel(panel, guiImages.get(0), 100, 100, 100, 100);
+        storeHouseImage = GUITools.createLabel(panel, player.storeHouse().PieceImage.pieceImage, 100, 100, 100, 100);
 
-        slotOne = GUITools.createLabel(panel, slotImages.get(0), 100, 100, 100, 100);
-        slotTwo = GUITools.createLabel(panel, slotImages.get(1), 100, 100, 100, 100);
-        slotThree = GUITools.createLabel(panel, slotImages.get(2), 100, 100, 100, 100);
-        slotFour = GUITools.createLabel(panel, slotImages.get(3), 100, 100, 100, 100);
-        slotFive = GUITools.createLabel(panel, slotImages.get(4), 100, 100, 100, 100);
-        slotSix = GUITools.createLabel(panel, slotImages.get(5), 100, 100, 100, 100);
-        slotSeven = GUITools.createLabel(panel, slotImages.get(6), 100, 100, 100, 100);
-        slotEight = GUITools.createLabel(panel, slotImages.get(7), 100, 100, 100, 100);
-        slotNine = GUITools.createLabel(panel, slotImages.get(8), 100, 100, 100, 100);
-        slotTen = GUITools.createLabel(panel, slotImages.get(9), 100, 100, 100, 100);
-        slotEleven = GUITools.createLabel(panel, slotImages.get(10), 100, 100, 100, 100);
-        slotTwelve = GUITools.createLabel(panel, slotImages.get(11), 100, 100, 100, 100);
+        slotOne = GUITools.createLabel(panel, player.storeHouse().get(0).PieceImage.pieceImage, 100, 100, 100, 100);
+        slotTwo = GUITools.createLabel(panel, player.storeHouse().get(1).PieceImage.pieceImage, 100, 100, 100, 100);
+        slotThree = GUITools.createLabel(panel, player.storeHouse().get(2).PieceImage.pieceImage, 100, 100, 100, 100);
+        slotFour = GUITools.createLabel(panel, player.storeHouse().get(3).PieceImage.pieceImage, 100, 100, 100, 100);
+        slotFive = GUITools.createLabel(panel, player.storeHouse().get(4).PieceImage.pieceImage, 100, 100, 100, 100);
+        slotSix = GUITools.createLabel(panel, player.storeHouse().get(5).PieceImage.pieceImage, 100, 100, 100, 100);
+        slotSeven = GUITools.createLabel(panel, player.storeHouse().get(6).PieceImage.pieceImage, 100, 100, 100, 100);
+        slotEight = GUITools.createLabel(panel, player.storeHouse().get(7).PieceImage.pieceImage, 100, 100, 100, 100);
+        slotNine = GUITools.createLabel(panel, player.storeHouse().get(8).PieceImage.pieceImage, 100, 100, 100, 100);
+        slotTen = GUITools.createLabel(panel, player.storeHouse().get(9).PieceImage.pieceImage, 100, 100, 100, 100);
+        slotEleven = GUITools.createLabel(panel, player.storeHouse().get(10).PieceImage.pieceImage, 100, 100, 100, 100);
+        slotTwelve = GUITools.createLabel(panel, player.storeHouse().get(11).PieceImage.pieceImage, 100, 100, 100, 100);
     }
 
     /**
     * updates the storehouse display based on the current contents of the player's storehouse
     */
     public void updateStoreHouse() {
-        slotOne.setIcon(new ImageIcon(slotImages.get(0)));
-        slotTwo.setIcon(new ImageIcon(slotImages.get(1)));
-        slotThree.setIcon(new ImageIcon(slotImages.get(2)));
-        slotFour.setIcon(new ImageIcon(slotImages.get(3)));
-        slotFive.setIcon(new ImageIcon(slotImages.get(4)));
-        slotSix.setIcon(new ImageIcon(slotImages.get(5)));
-        slotSeven.setIcon(new ImageIcon(slotImages.get(6)));
-        slotEight.setIcon(new ImageIcon(slotImages.get(7)));
-        slotNine.setIcon(new ImageIcon(slotImages.get(8)));
-        slotTen.setIcon(new ImageIcon(slotImages.get(9)));
-        slotEleven.setIcon(new ImageIcon(slotImages.get(10)));
-        slotTwelve.setIcon(new ImageIcon(slotImages.get(11)));
+        slotOne.setIcon(new ImageIcon(player.storeHouse().get(0).PieceImage.pieceImage));
+        slotTwo.setIcon(new ImageIcon(player.storeHouse().get(1).PieceImage.pieceImage));
+        slotThree.setIcon(new ImageIcon(player.storeHouse().get(2).PieceImage.pieceImage));
+        slotFour.setIcon(new ImageIcon(player.storeHouse().get(3).PieceImage.pieceImage));
+        slotFive.setIcon(new ImageIcon(player.storeHouse().get(4).PieceImage.pieceImage));
+        slotSix.setIcon(new ImageIcon(player.storeHouse().get(5).PieceImage.pieceImage));
+        slotSeven.setIcon(new ImageIcon(player.storeHouse().get(6).PieceImage.pieceImage));
+        slotEight.setIcon(new ImageIcon(player.storeHouse().get(7).PieceImage.pieceImage));
+        slotNine.setIcon(new ImageIcon(player.storeHouse().get(8).PieceImage.pieceImage));
+        slotTen.setIcon(new ImageIcon(player.storeHouse().get(9).PieceImage.pieceImage));
+        slotEleven.setIcon(new ImageIcon(player.storeHouse().get(10).PieceImage.pieceImage));
+        slotTwelve.setIcon(new ImageIcon(player.storeHouse().get(11).PieceImage.pieceImage));
     }
 
                                         /* Getters / Setters / ToString */
