4ac4f7b24deeeeccfbae5497edea3ea7afeef971
c-miller <caroline.miller24@bishopireton.org>
2023-06-05 13:31:49 -0400

    added JLabels for the different resources

--- src/gui/PlayerResources.java
+++ src/gui/PlayerResources.java
@@ -2,17 +2,14 @@ package src.gui;
 
 /*
     @author Caroline Miller
-    PlayerResources: 
+    PlayerResources: GUI Representation of the Player's Resources (houses + sestertii)
  */
 
 import src.game.player.Player;
 
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+
 
 public class PlayerResources {
     private Player player; //The specific player whose resources these are
@@ -22,58 +19,73 @@ public class PlayerResources {
 
     private JPanel panel; //JPanel
 
-    //private JLabel sestertiiLabel;
-    //private JLabel housesLabel;
-
-    private ButtonGroup resourcesFilter;
-    private JRadioButton sestertiiButton; //shows the sestertii the player has
-    private JRadioButton houseButton; //shows the houses the player has 
-
+    private JLabel sestertiiLabel;
+    private JLabel housesLabel;
+ 
+    
+    /**
+     * Contructor
+     * @param player the player
+     * @param sestertii amount of money the player has
+     * @param numHouses number of houses the player has
+     * @param panel panel that holds the graphics
+     */
     public PlayerResources(Player player, int sestertii, int numHouses, JPanel panel) {
         this.player = player;
         this.sestertii = sestertii;
         this.numHouses = numHouses;
         this.panel = panel;
 
-        createJRadioButtons();
+        createJLabels();
+        addingJLabels();
     }
 
-    public void createJRadioButtons(){
-        resourcesFilter = new ButtonGroup();
+    /** 
+     * creates JLabels
+    */
+    public void createJLabels() {
+        sestertii = getSestertii();
+        numHouses = getHouses();
+        
+        sestertiiLabel = new JLabel("Sestertii Amount = " + sestertii, JLabel.LEFT); //add where the label will go
+        housesLabel = new JLabel("Number of Houses = " + numHouses, JLabel.RIGHT); //add where the label will go
 
-        //TBD add images, maybe
-        sestertiiButton = new JRadioButton("Sestertii :)"); 
-        houseButton = new JRadioButton("Houses :)");
-
-        resourcesFilter.add(sestertiiButton); //will add an image for the button 
-        resourcesFilter.add(houseButton); //will add an image for the button
-
-        sestertiiButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Show sestertii amount");
-            }
-        });
-
-        houseButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Show house amount");
-            }
-        });
+        sestertiiLabel.setSize(100,100); //width + height will change
+        housesLabel.setSize(100,100); //width + height will change
     }
 
-    //return Player
+    /**
+     * adds JLabels to the JPanel
+     */
+    public void addingJLabels() {
+        panel.add(sestertiiLabel);
+        panel.add(housesLabel);
+    }
+
+    /* 
+    public void updateJLabels() {
+        sestertiiLabel.setText("Sestertii Amount = " + getSestertii());
+        housesLabel.setText("Number of Houses = " + getHouses());
+    }
+    */
+        
+    /**
+     * @return player
+     */
     public Player getPlayer() {
        return player;
     }
 
-    //return sestertii amount
+    /**
+     * @return sestertii
+     */
     public int getSestertii() {
          return sestertii;
     }
 
-    //return house amount
+    /**
+     * @return numHouses
+     */
     public int getHouses() {
         return numHouses;
     }
