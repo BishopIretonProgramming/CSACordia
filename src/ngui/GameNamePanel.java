f77b9871b15f34983e8190ee9248b6ca586363ce
devinlinux <ssmith969898@gmail.com>
2023-06-07 11:05:04 -0400

    Made GameNamePanel look better

--- src/ngui/GameNamePanel.java
+++ src/ngui/GameNamePanel.java
@@ -2,61 +2,43 @@ package src.ngui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import src.game.Game;
 
 public class GameNamePanel extends JPanel {
-    private JTextField nameField;
+    
+    private JTextField field;
     private JButton startButton;
+
     private Frame frame;
     private Game game;
 
     public GameNamePanel(Frame frame, Game game) {
         this.frame = frame;
         this.game = game;
-        setLayout(new BorderLayout());
 
-        nameField = new JTextField(20);
-        nameField.setFont(nameField.getFont().deriveFont(60f));
-        startButton = new JButton("Start Game");
-        startButton.setEnabled(false);
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 10, 10, 10);
 
-        startButton.addActionListener(e -> {
-            String gameName = nameField.getText();
-            if (!gameName.isEmpty()) {
-                game.setName(gameName);
-                frame.showMainPanel(game);
+        this.field = new JTextField(20);
+        this.startButton = new JButton("Start Game");
+
+        this.startButton.addActionListener(e -> {
+            String name = this.field.getText();
+            if (!name.isEmpty()) {
+                this.game.setName(name);
+                this.frame.showMainPanel(game);
             }
         });
 
-        JPanel panel = new JPanel(new BorderLayout());
-        panel.add(nameField, BorderLayout.CENTER);
-        panel.add(startButton, BorderLayout.EAST);
+        add(this.field, gbc);
 
-        add(panel, BorderLayout.CENTER);
+        gbc.gridx = 1;
+        gbc.anchor = GridBagConstraints.WEST;
 
-        nameField.getDocument().addDocumentListener(new javax.swing.event.DocumentListener() {
-            @Override
-            public void insertUpdate(javax.swing.event.DocumentEvent e) {
-                validateButton();
-            }
-
-            @Override
-            public void removeUpdate(javax.swing.event.DocumentEvent e) {
-                validateButton();
-            }
-
-            @Override
-            public void changedUpdate(javax.swing.event.DocumentEvent e) {
-                validateButton();
-            }
-        });
-    }
-
-    private void validateButton() {
-        String gameName = nameField.getText();
-        startButton.setEnabled(!gameName.isEmpty());
+        add(this.startButton, gbc);
     }
 }
